baeaeca4e531424ab3709a88aaa76e834c380cb7
Paulinguis993 <99148906+Pau993@users.noreply.github.com>
2025-03-13 21:39:39 -0500

    Delete Taller06 directory

--- Taller06/src/main/java/com/example/property/PropertyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.property;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PropertyRepository extends JpaRepository<Property, Long> {
-}
